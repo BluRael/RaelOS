3fb73bfe2a4369b80d7409759ca9adb7243b6cf4
BluRael <alunblue14@icloud.com>
2019-12-24 21:52:23 +0000

    Created testing code for keyboard IO method

---
+++ RAELOS/Tests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RAELOS
+{
+    static class Tests
+    {
+        public static void KeyboardFeedback1()
+        {
+            Console.Clear();
+            Console.SetCursorPosition(0, 4);
+            Console.WriteLine("Kernal feedback test 1\n");
+            Console.WriteLine("K Press:");
+            Console.WriteLine("C Output:");
+            while (true)
+            {
+                Console.SetCursorPosition(10, 6);
+                string c = Console.ReadKey().KeyChar.ToString();
+                Console.SetCursorPosition(10, 7);
+                Console.Write(c + " ");
+            }
+        }
+        public static void KeyboardTypeOut()
+        {
+            Console.Clear();
+            while (true)
+            {
+                string c = Console.ReadKey(true).KeyChar.ToString();
+                Console.Write(c);
+            }
+        }
+        public static void NoDebug()
+        {
+            //Placeholder sub, this does nothing and proceeds to boot RAELOS
+        }
+    }
+}
