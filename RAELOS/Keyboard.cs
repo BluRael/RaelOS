dd9e02ab9af53ccdf94a169d84a9ea39a751c426
BluRael <alunblue14@icloud.com>
2019-12-24 21:51:49 +0000

    Added placeholder file for keyboard input manager

---
+++ RAELOS/Keyboard.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RAELOS
+{
+    class Keyboard
+    {
+        /*FIELDS TO PLOW
+         * 
+         */
+        /*THREADS TO STITCH
+         * 
+         */
+        /*SUBS TO BOT:
+         * 
+         */
+    }
+}
